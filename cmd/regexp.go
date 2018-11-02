4474f6bc458751a069ea4fafa5e418f01f7065c7
Mihail Bodrov <connormiha1@gmail.com>
2018-11-02 03:52:21 +0300

    Add wildcard check

---
+++ cmd/regexp.go
@@ -0,0 +1,35 @@
+package main
+
+// IsMatch regexp with ".", "*" supports
+func IsMatch(text, pattern string) bool {
+	if text == "" {
+		// '' and ''
+		if pattern == "" {
+			return true
+		}
+		// '' and 'a*'
+		return len(pattern) == 2 && pattern[1] == '*'
+	}
+
+	// 'a' and ''
+	if pattern == "" {
+		return false
+	}
+	//  'a' and 'b*'
+	if len(pattern) > 1 && pattern[1] == '*' {
+		return IsMatch(text, pattern[2:]) || IsMatch(text[1:], pattern)
+	}
+
+	//  'a' and 'a'
+	if text[0] == pattern[0] {
+		return IsMatch(text[1:], pattern[1:])
+	}
+
+	//  'a' and '.'
+	if pattern[0] == '.' {
+		return IsMatch(text[1:], pattern[1:])
+	}
+
+	//  'a' and 'b'
+	return false
+}
---
+++ cmd/regexp_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIsMatch(t *testing.T) {
+	var result bool
+	var invalidValues = [][2]string{
+		[2]string{"aa", "ab"},
+		[2]string{"ba", "ab"},
+		[2]string{"ad", "ac"},
+		[2]string{"ae", "ad"},
+		[2]string{"aa", "a*b"},
+	}
+
+	for _, value := range invalidValues {
+		result = IsMatch(value[0], value[1])
+
+		if result {
+			t.Error("Should be invalid ", value)
+		}
+	}
+
+	var validValues = [][2]string{
+		[2]string{"aa", "aa"},
+		[2]string{"", ""},
+		[2]string{"ad", "ac*d"},
+		[2]string{"ad", "a.*d"},
+		[2]string{"ae", "a*d*e*"},
+		[2]string{"aa", "a*b*a"},
+		[2]string{"aa", ".."},
+		[2]string{"aa", "..*"},
+		[2]string{"", ".*"},
+		[2]string{"abcdefgz", ".*"},
+		[2]string{"abcdefgz", ".*e*"},
+	}
+
+	for _, value := range validValues {
+		result = IsMatch(value[0], value[1])
+
+		if !result {
+			t.Error("Should be valid ", value)
+		}
+	}
+}
